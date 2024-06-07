ccc7fafb7428b7c77acdec9e1d4aa5cb8d32f2a1
Karimzadeh, Kavian <kavian.karimzadeh@flatexdegiro.com>
2024-06-07 12:20:48 +0200

    updating package versions

--- variables.tf
+++ variables.tf
@@ -11,15 +11,11 @@ variable "instances" {
 }
 
 variable "server_type" {
-  default = "cx21"
+  default = "cx22"
 }
 
 variable "os_type" {
   default = "alma-8"
-}
-
-variable "disk_size" {
-  default = "20"
 }
 
 variable "services_protocol" {
@@ -42,6 +38,49 @@ variable "services_workers_port_2" {
   default = "443"
 }
 
+variable "remote_usr" {
+  default = "itman"
+}
+
+variable "home_dir" {
+  default = "/home/itman"
+}
+
+variable "cri_socket" {
+  default = "unix:///var/run/containerd/containerd.sock"
+}
+
+variable "cluster_name" {
+  default = "cluster.local"
+}
+
+variable "kubernetes_version" {
+  default = "1.30.1"
+}
+
+variable "containerd_version" {
+  default = "1.6.32-3.1.el8"
+}
+
+variable "haproxy_version" {
+  default = "1.8.27-5.el8"
+}
+
+variable "keepalived_version" {
+  default = "2.1.5-9.el8"
+}
+
+variable "cilium_version" {
+  default = "1.15.5"
+}
+
+variable "ingress_nginx_version" {
+  default = "4.10.1"
+}
+
+variable "helm_version" {
+  default = "3.15.1"
+}
 variable "ip_range" {
   default = "10.0.1.0/24"
 }
@@ -54,52 +93,8 @@ variable "lb_workers_private_ip" {
   default = "10.0.1.21"
 }
 
-variable "remote_usr" {
-  default = "itman"
-}
-
-variable "home_dir" {
-  default = "/home/itman"
-}
-
-variable "kubernetes_version" {
-  default = "1.26.4"
-}
-
-variable "helm_version" {
-  default = "3.11.3"
-}
-
-variable "containerd_version" {
-  default = "1.6.21-3.1.el8"
-}
-
-variable "cilium_version" {
-  default = "1.13.2"
-}
-
-variable "cri_socket" {
-  default = "unix:///var/run/containerd/containerd.sock"
-}
-
 variable "pod_subnet" {
   default = "10.244.0.0/20"
-}
-
-variable "cluster_name" {
-  default = "cluster.local"
-}
-
-variable "haproxy_version" {
-  default = "1.8.27-5.el8"
-}
-
-variable "keepalived_version" {
-  default = "2.1.5-9.el8"
-}
-
-variable "ingress_nginx_version" {
-  default = "4.6.1"
 }
 
 variable "eth_master01" {
