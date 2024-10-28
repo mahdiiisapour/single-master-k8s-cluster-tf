4d04b8cbaeab32f7bc53dac12f32e5e343e6cb09
kaviankarimzadeh <kavian.karimzadeh@hotmail.com>
2024-10-28 20:09:20 +0100

    update provider version

--- provider.tf
+++ provider.tf
@@ -4,6 +4,14 @@ terraform {
       source = "hetznercloud/hcloud"
       version = "1.48.1"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.3"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
+    }
   }
 }
 
